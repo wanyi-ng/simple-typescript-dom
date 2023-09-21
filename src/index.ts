096507c6eae7cc0e76e2e928c43c2f4f02f380f9
Wanyi <wanyi.ng1975@gmail.com>
2023-09-20 22:44:09 -0400

    add interface Todo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-const todos = document.querySelector("ul")!;
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
+const list = document.querySelector("ul")!;
 const btn = document.getElementById("todo-btn")! as HTMLButtonElement;
 const input = document.getElementById("todo-input")! as HTMLInputElement;
 const form = document.querySelector("form")!;
@@ -6,17 +13,24 @@ const form = document.querySelector("form")!;
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
 
-  const newTodo = input.value;
-  const li = document.createElement("li");
-  const checkbox = document.createElement("input");
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
 
-  checkbox.type = "checkbox";
-  li.textContent = newTodo;
-  li.prepend(checkbox);
-
-  todos.appendChild(li);
+  createTodoElement(newTodo);
+  todos.push(newTodo);
 
   input.value = "";
 };
+
+function createTodoElement(todo: Todo) {
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  li.textContent = todo.text;
+  li.prepend(checkbox);
+  list.appendChild(li);
+}
 
 form.addEventListener("submit", handleSubmit);
